5b0722fde56cd28be271d91162f25adc96f4b47a
Jun Zhao <jun.zhao@intel.com>
2017-03-19 13:42:06 +0800

    Add xingxiao msg send

--- src/btgattclient.c
+++ src/btgattclient.c
@@ -572,8 +572,8 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, u_char *packet)
 	}
 
         if (match("Services", payload)) {
-                char *mac = ethernet->ether_shost;
-                printf("src mac %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+                u_char *mac = ethernet->ether_shost;
+                printf("src mac %02.2x:%02.2x:%02.2x:%02.2x:%02.2x:%02.2x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
                 uint8_t value[128] =  {0};
                 int cfd = create_client_sock(CLIENT);
                 snprintf(value, 127, "%02x:%02x:%02x:%02x:%02x:%02x SNIFFER\n",
